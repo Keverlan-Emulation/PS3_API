2629616db8ca1300ee81d17606ec26c52ebdbfe0
James Farewell <83976529+James-FarewellCodes@users.noreply.github.com>
2021-05-10 23:20:26 -0400

    Add files via upload

---
+++ sce_virtual_memory.cpp
@@ -0,0 +1,9 @@
+namespace sce_virtual_memory
+
+end namespace
+
+
+     const g_stat_addr = memory_reserve_1GiB(g_hook_addr);
+     const g_stat_addr = memory_reserve_2GiB(g_hook_addr);
+
+     void: const g_stat_addr = video_reserve_500MBiT;
